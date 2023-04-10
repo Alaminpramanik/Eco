0411df89f5e98fcedfbaa737ec9af2a5ae20ae59
alaminpramanik <alamin493641@gmail.com>
2023-04-10 13:01:58 +0600

    footer and app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import {BsSearch} from 'react-icons/bs'
 
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <>
+            <footer className='py-4'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                
+                        <div className='col-3'>
+                            {/* <h4 className='text-center all-text mb-4'>About Us</h4> */}
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='all-text py-2 mb-1'>about us</Link>
+                                <Link className='all-text py-2 mb-1 '>contact us</Link>
+                            </div>
+                        </div>
+                        <div className='col-3'>
+                            {/* <h4 className='text-center all-text mb-4'>About Us</h4> */}
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='all-text py-2 mb-1'>Terms and conditions</Link>
+                                <Link className='all-text py-2 mb-1 '>Privacy Policy</Link>
+                                <Link className='all-text py-2 mb-1 '>Payment System</Link>
+                                <Link className='all-text py-2 mb-1 '>Delivery Area</Link>
+                            </div>
+                        </div>
+                        <div className='col-3'>
+                            {/* <h4 className='text-center all-text mb-4'>About Us</h4> */}
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='all-text py-2 mb-1'>Security</Link>
+                                <Link className='all-text py-2 mb-1 '>Download Apps</Link>
+                                <Link className='all-text py-2 mb-1 '>Careers</Link>
+                                
+                            </div>
+                        </div>
+                        <div className='col-3'>
+                            <h4 className='text-center all-text mb-4'>Social Media</h4>
+
+                        </div>
+
+                </div>
+                </div>
+                
+            </footer>
+            <footer className='footer py-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                
+                        <div className='col-12'>
+                            <p className='text-center all-text mb-4'>Developed by @alamin</p>
+                            
+                        </div>
+                    
+
+                    </div> 
+                </div>
+                
+            </footer>
+        </>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,11 +15,12 @@ import fishbeef from "../images/fishbeef.jpg"
 import Vegetable from "../images/vegetable.jpg"
 import drink from "../images/drink.jpeg"
 import Choclate from "../images/choclates.jpg"
-
+import background from "../images/apps.png"
+import gplaystore from "../images/playstore.png"
 
 function Home() {
     return (
-        <div>
+        <>
             <header className='header-body py-3'>
                 <div className='container-xxl'>
                     <div className='row'>
@@ -53,7 +54,7 @@ function Home() {
                             </div>
 
                             <div className='row '>
-                                <div className='customer-review'>
+                                <div className='product'>
                                     <div className='col-12'>
                                         <div className='row'>
                                             <div className='col-12 '>
@@ -63,57 +64,59 @@ function Home() {
                                     </div>
                                 </div>
                             </div>
-                            <div className='row '>
-                                <div className='col-3'>
-                                    <div>
-                                        <img className='product-items' src={fishbeef} />
-                                    </div>
-                                    
-                                    <div className='row'>
-                                        <div className='col-12  '>
-                                            <button className=' add-butoon text-white'> See List</button>
+                            <div className='row'>
+                                {/* <div className='product-listing'> */}
+                                    <div className='col-3'>
+                                        <div>
+                                            <img className='product-items' src={fishbeef} />
                                         </div>
+                                        
+                                        <div className='row'>
+                                            <div className='col-12  '>
+                                                <button className=' add-butoon text-white'> See List</button>
+                                            </div>
+                                        </div>
+                                        
                                     </div>
-                                    
-                                </div>
 
-                                <div className='col-3'>
-                                    <div>
-                                        <img className='product-items' src={Vegetable} />
+                                    <div className='col-3'>
+                                        <div>
+                                            <img className='product-items' src={Vegetable} />
+                                        </div>
+                                        
+                                        <div className='row'>
+                                            <div className='col-12  '>
+                                                <button className=' add-butoon text-white'> See List</button>
+                                            </div>
+                                        </div>
+                                        
                                     </div>
                                     
-                                    <div className='row'>
-                                        <div className='col-12  '>
-                                            <button className=' add-butoon text-white'> See List</button>
+                                    <div className='col-3 '>
+                                        <div className='row'>
+                                            <div className='col-12'>
+                                                <img className='product-items' src={drink} />
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-12  '>
+                                                <button className=' add-butoon text-white'> See List</button>
+                                            </div>
                                         </div>
                                     </div>
-                                    
-                                </div>
-                                
-                                <div className='col-3 '>
-                                    <div className='row'>
-                                        <div className='col-12'>
-                                            <img className='product-items' src={drink} />
+                                    <div className='col-3 '>
+                                        <div className='row'>
+                                            <div className='col-12'>
+                                                <img className='product-items' src={Choclate} />
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-12  '>
+                                                <button className=' add-butoon text-white'> See List</button>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className='row'>
-                                        <div className='col-12  '>
-                                            <button className=' add-butoon text-white'> See List</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='col-3 '>
-                                    <div className='row'>
-                                        <div className='col-12'>
-                                            <img className='product-items' src={Choclate} />
-                                        </div>
-                                    </div>
-                                    <div className='row'>
-                                        <div className='col-12  '>
-                                            <button className=' add-butoon text-white'> See List</button>
-                                        </div>
-                                    </div>
-                                </div>
+                                {/* </div> */}
                             </div>
 
                             <div className='row '>
@@ -124,14 +127,46 @@ function Home() {
                                                 <h2 className='all-text'>Customer Reviews</h2>
                                             </div>
                                         </div>
+                                        <div className='row'>
+                                            <div className='col-12 '>
+                                                <p className='all-text'>Overall, I'm very happy with my experience using Shopify for my eCommerce website. It's been an essential tool in growing my business and I would highly recommend it to anyone looking to start an online store.</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+                            <div className='row '>
+                                <div className='android-app' style={{backgroundImage:`url(${background})`, backgroundRepeat: 'no-repeat',}}>
+                                    <div className='col-12'>
+                                        <div className='row'>
+                                            <div className='col-12'>
+                                                <h2 className='text-white fs-1'>Download Mobile App</h2>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-12'>
+                                                <button className='text-white app-download fs-1'>Download</button>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                          
+                                            <div className='col-12'>
+                                                <p className='text-white'>Get it On <br/> Google Play Store</p>
+                                                <img className='playstore' src={gplaystore} />
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            
                         </div>
                     </div>
                 </div>
             </header>
-        </div>
+        </>
     );
 }
 
